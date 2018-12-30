fac2b17b2413bc17f12de7c32b12da3c4938819b
shahmas6 <shahbazmasud@hotmail.com>
2018-12-30 15:13:28 -0500

    Update p1.m

--- p1.m
+++ p1.m
@@ -1,6 +1,7 @@
 %{
-  2-Class classifier
-  
+  Takes input samples X with their true labels Y and two specified labels l1 and l2
+  returns only those smaples in X and labels in Y that correspond to l1 and l2
+  output labels renamed to 1 and 2
 %}
 function [Xout, Yout] = p1(X,Y,l1,l2)
 
